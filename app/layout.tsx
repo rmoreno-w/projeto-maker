637b84927e7531830c101b258e88961fbe83382a
Rodrigo Moreno <rmoreno.w@gmail.com>
2023-11-03 17:06:33 -0300

    Pagina inicial

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang='en' className='bg-[#FAF8F2] text-black'>
+        <html lang='en' className='bg-makerBg text-black'>
             <body className={inter.className}>{children}</body>
         </html>
     );
