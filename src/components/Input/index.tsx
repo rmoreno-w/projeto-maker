b965c8734fb59e7f78f7d0ee42e777b70d8b720c
Rodrigo Moreno <rmoreno.w@gmail.com>
2023-11-06 03:50:13 -0300

    Tornando componente input dinamico

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,13 +1,21 @@
+import { Dispatch, SetStateAction } from 'react';
+
 interface InputProps {
     label: string;
+    currentValue: string;
+    onChangeValue: Dispatch<SetStateAction<string>>;
+    disabled?: boolean;
 }
 
-export function Input({ label }: InputProps) {
+export function Input({ label, currentValue, onChangeValue, disabled = false }: InputProps) {
     return (
         <label className='flex flex-col gap-2'>
             {label}
             <input
+                disabled={disabled}
                 type={label == 'Email' ? 'email' : 'text'}
+                value={currentValue}
+                onChange={(event) => onChangeValue(event.target.value)}
                 className='border-2 bg-transparent border-gray-300 rounded-lg h-12 transition-all duration-300 focus:outline-none focus:ring focus:ring-makerYellow focus:ring-offset-2 focus:ring-offset-makerBg focus:border-makerYellow p-2'
             />
         </label>
