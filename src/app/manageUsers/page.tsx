33322ca65858b6efd2b5a2b97abddbf47bf0ec83
Rodrigo Moreno <rmoreno.w@gmail.com>
2023-11-12 14:44:39 -0300

    Separando membros e clientes em gerenciar users

--- src/app/manageUsers/page.tsx
+++ src/app/manageUsers/page.tsx
@@ -21,7 +21,8 @@ interface User {
 }
 export default function ManageUsers() {
     const { authData } = useAuth();
-    const [users, setUsers] = useState<User[]>([]);
+    const [customers, setCustomers] = useState<User[]>([]);
+    const [members, setMembers] = useState<User[]>([]);
 
     useEffect(() => {
         apiClient
@@ -32,14 +33,18 @@ export default function ManageUsers() {
             })
             .then((response) => {
                 console.log(response.data);
-                setUsers(response.data);
+                let customers = response.data.filter((customer) => customer.role.includes('customer'));
+                let members = response.data.filter((customer) => customer.role.includes('member'));
+                setCustomers(customers);
+                setMembers(members);
             })
             .catch((e) => {
                 console.log(e);
             });
     }, []);
 
-    async function removeUser(userId: number) {
+    async function removeUser(userId: number, userType: string) {
+        console.log(`id: ${userId}, type: ${userType}`);
         apiClient
             .delete(`/users/${userId}`, {
                 headers: {
@@ -48,9 +53,18 @@ export default function ManageUsers() {
             })
             .then((result) => {
                 console.log(result);
-                const updatedUsersList = users.filter((user) => user.id != userId);
-                setUsers(updatedUsersList);
-                // setAreFieldsEdited(false);
+
+                if (userType == 'member') {
+                    const updatedMembersList = members.filter((member) => member.id != userId);
+                    console.log(typeof updatedMembersList);
+                    setMembers(updatedMembersList);
+                    // setAreFieldsEdited(false);}
+                } else if (userType == 'customer') {
+                    const updatedCustomersList = customers.filter((customer) => customer.id != userId);
+                    console.log(typeof updatedCustomersList);
+                    setCustomers(updatedCustomersList);
+                    // setAreFieldsEdited(false);}
+                }
             })
             .catch((e) => console.log(e));
     }
@@ -58,25 +72,48 @@ export default function ManageUsers() {
     return (
         <>
             <main className=' bg-makerBg border-4 border-makerYellow mx-[120px] my-[60px] rounded-3xl overflow-hidden'>
-                <div className='py-10 px-24 flex flex-col justify-center items-center gap-3'>
-                    {users ? (
+                <div className='py-10 px-24 flex flex-col justify-center gap-3'>
+                    {members.length != 0 ? (
                         <div className='flex flex-col gap-4'>
-                            {users.map((user) => (
-                                <ul key={user.id} className='flex gap-2 pl-4 h-10 items-center'>
+                            <h2 className='text-[20px] font-semibold'>Membros</h2>
+                            {members.map((member) => (
+                                <ul key={member.id} className='flex gap-2 pl-4 h-10 items-center'>
                                     <div className='bg-makerYellow h-2 w-2 rounded' />
-                                    <li>Nome: {user.name}</li>
-                                    <li>Email: {user.email}</li>
+                                    <li>Nome: {member.name}</li>
+                                    <li>Email: {member.email}</li>
                                     <button
-                                        className='bg-makerYellow px-2 py-1 rounded'
-                                        onClick={() => removeUser(user.id)}
+                                        className='bg-[#BC2119] px-2 py-1 rounded text-makerBg'
+                                        onClick={() => removeUser(member.id, 'member')}
                                     >
-                                        Remover Usuário
+                                        Deletar Membro
                                     </button>
                                 </ul>
                             ))}
                         </div>
                     ) : (
-                        <p>Ainda não há usuários cadastrados</p>
+                        <p>Ainda não há membros cadastrados</p>
+                    )}
+                </div>
+                <div className='py-10 px-24 flex flex-col justify-center gap-3'>
+                    {customers.length != 0 ? (
+                        <div className='flex flex-col gap-4'>
+                            <h2 className='text-[20px] font-semibold'>Clientes</h2>
+                            {customers.map((customer) => (
+                                <ul key={customer.id} className='flex gap-2 pl-4 h-10 items-center'>
+                                    <div className='bg-makerYellow h-2 w-2 rounded' />
+                                    <li>Nome: {customer.name}</li>
+                                    <li>Email: {customer.email}</li>
+                                    <button
+                                        className='bg-[#BC2119] px-2 py-1 rounded text-makerBg'
+                                        onClick={() => removeUser(customer.id, 'customer')}
+                                    >
+                                        Deletar Cliente
+                                    </button>
+                                </ul>
+                            ))}
+                        </div>
+                    ) : (
+                        <p>Ainda não há clientes cadastrados</p>
                     )}
                 </div>
             </main>
